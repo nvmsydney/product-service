ea8762f8eebdbd814b4098008ad5b09b79b91a0c
Sydney Nguyen <melosydrose@gmail.com>
2023-09-14 19:39:41 -0400

    uncomment db

--- product_service.py
+++ product_service.py
@@ -49,6 +49,6 @@ def add_product():
 
 
 if __name__ == '__main__':
-    #with app.app_context():
-        #db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
